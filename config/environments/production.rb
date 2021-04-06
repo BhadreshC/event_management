18ba437561b5276130024bfd4a9127ab15715f33
BhadreshC <wc.bhadresh@gmail.com>
2021-04-06 15:40:40 +0530

    smtp setting change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -122,10 +122,11 @@ Rails.application.configure do
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port:    "587",
+    domain:  "gmail.com",
+    port:    587,
     user_name: ENV['EMAIL'],
     password: ENV['PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto:  true
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 end
